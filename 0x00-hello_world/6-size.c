3cb49c9e7317e3b49c3015e7083273babfaafe4c
Serahwanjiku <sarahmucugi@gmail.com>
2023-03-16 12:38:45 -0700

    yesss

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,3 +13,4 @@ printf("Size of a long int: %lu byte(s)", sizeof(long int));
 printf("size of a long long int: %lu byte(s)", sizeof(long long int));
 printf("size of a float: %lu byte(s)", sizeof(float));
 return (0);
+}
