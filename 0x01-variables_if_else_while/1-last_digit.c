fbc7ea0a893568ef81902593a20d75f38f62c07e
Serahwanjiku <sarahmucugi@gmail.com>
2023-03-17 13:46:26 -0700

    dn

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,6 @@ else if (m == 0)
 printf("last digit of %d is %d and is 0", n, m);
 else if (m < 6 && m != 0)
 printf("last digit of %d is %d and is less than 6 and not 0", n, m);
-printf('\n');
+printf("\n");
 return (0);
 }
