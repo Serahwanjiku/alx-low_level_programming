5cc5cb2a2523d6f71d04b89ffe6beb365e775a71
Serahwanjiku <sarahmucugi@gmail.com>
2023-03-17 13:09:32 -0700

    slp

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this is my main
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar("\n");
+return (0);
+}
