45a733b3e8827bf7dffece9d5e6043586e5910f6
Serahwanjiku <sarahmucugi@gmail.com>
2023-03-23 10:36:11 -0700

    d

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,6 +8,7 @@
  * @c: third integer
  * Return: largest number
  */
+/**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
 * @month: month in number format
