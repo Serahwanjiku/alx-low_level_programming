67d9479e9cc21a11db3ee8fd5c1969d56763ff57
Serahwanjiku <sarahmucugi@gmail.com>
2023-03-24 09:58:26 -0700

    numb

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,20 @@
+
 #include "main.h"
+
 /**
- * print_number - print 0-9
+ * print_numbers - print 0-9
  * return: void
  */
-void print_number(void)
+
+void print_numbers(void)
 {
-char c:
-for (c = '0' ; c <= '9' ; c++
+char c;
+
+for (c = '0'; c <= '9'; c++)
 {
 _putchar(c);
 }
 _putchar('\n');
 }
+
 
