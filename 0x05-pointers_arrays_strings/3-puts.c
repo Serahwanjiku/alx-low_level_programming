d4a82c61e2ba18c203e80114ee7ad78bb598a4c4
Serahwanjiku <sarahmucugi@gmail.com>
2023-03-27 10:46:11 -0700

    num3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ *@str: function parameter
+ * Return: 0 always
+ */
+void _puts(char *str)
+{
+int c;
+for (c = 0 ; str[c] != '\0' ;  c++)
+_putchar(str[c]);
+_putchar('\n');
+}
