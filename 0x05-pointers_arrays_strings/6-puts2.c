b1690da731db6ea00f8906bbdd771c01138aa44e
Serahwanjiku <sarahmucugi@gmail.com>
2023-03-27 14:33:06 -0700

    uts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,20 @@
  */
 void puts2(char *str)
 {
-int i;
-for (i = 0 ; str[i] != '\0' ; i++)
+int longl = 0;
+int t = 0;
+char *y = str;
+int o;
+while (*y l= '\0')
 {
-_putchar(str[i]);
-i++;
+y++;
+longl++;
+}
+t = longl - 1;
+for (o = 0; 0 <= t ; o++)
+{
+if (o % 2 == 0)
+_putchar(str[o]);
 }
 _putchar('\n');
 }
