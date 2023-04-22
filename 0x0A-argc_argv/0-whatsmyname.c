d9299fdbd069b3e367e4f6eb127cac3e255e3c0d
Serahwanjiku <sarahmucugi@gmail.com>
2023-04-22 12:38:41 -0700

    pauuuuuuuuuuuuul wake up

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,15 @@
 #include "main.h"
 
 /*
- * main- prints the name of the program
- * @argc: counts the number
- * @argv: arguments array
- * Return: Always 0 (Success)
+ *
+ * main - this program prints the argv[0]
+(* a blank line
+*@argc: this is the quantity of parameters
+*@argv: the parameter to put in the pogram.
+* Description: this program prints the argv[0])?
+* Return: return 0 or 1
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - this program prints the number of elements
+(* a blank line
+*@argc: this is the quantity of parameters
+*@argv: the parameter to put in the program.
+* Description: this program prints the number of elements)?
+* Return: return 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
